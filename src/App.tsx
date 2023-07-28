78baa2d399a92d1cccbfbecc00e62dcdaa17dea5
Murat Can <muratal0606@gmail.com>
2023-07-28 13:45:49 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Text>EstimateME</Text>
+    </ChakraProvider>
+  );
+}
+
+export default App;
