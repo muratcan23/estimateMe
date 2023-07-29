cba6ff7d1894be05d0c4ed5cc836a0eacdb4803c
Murat Can <muratal0606@gmail.com>
2023-07-29 15:28:06 +0300

    add navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <Flex
+      w="100%"
+      h="10vh"
+      whiteSpace="-moz-pre-wrap"
+      bg="#1E1914"
+      justifyContent="center"
+    >
+      <Flex>
+        <Text
+          fontFamily="monospace"
+          fontSize="44px"
+          letterSpacing="2px"
+          color="#D2D8E2"
+        >
+          Estimate
+        </Text>
+        <Text fontFamily="heading" fontSize="25px" color="#CFD746">
+          Me
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Navbar;
